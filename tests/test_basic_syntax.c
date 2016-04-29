9f651c21c3622c3d9664c61c60e079d11fc4cc66
Quoc-Nghia NGUYEN <quoc-nghia.nguyen@etu.u-bordeaux.fr>
2016-04-29 15:28:38 +0200

    merge avec tests

--- tests/test_basic_syntax.c
+++ tests/test_basic_syntax.c
@@ -5,9 +5,11 @@ void test(void) {
 
     //on attend que la lecture de ce fichier NE RENVOIE PAS d'erreur -> donc ca PASS
     f = fopen("lex_test/basicTestOk.txt", "r");
-    expect_ok(scan_file(f), "Parse foret.txt");
+    expect_ok(scan_file(f), "Parse basicTestOk.txt");
     fclose(f);
 
+    expect_ok(scan_string("div{f arg1 arg2, \"text\"}"),        "Parse a good string, error on ERRtest 11");
+    
     //on attend que la lecture de ces chaines renvoient une erreur -> donc ca PASS
     expect_err(scan_string("div{{\"test\"}"), 	           	"Parse a bad string, error on ERRtest 1");
     expect_err(scan_string("_{\"yo\"}"),              	 	"Parse a bad string, error on ERRtest 2");
@@ -19,5 +21,4 @@ void test(void) {
     expect_err(scan_string("div{a\n[test=\"test\"]}"),      "Parse a bad string, error on ERRtest 8");
     expect_err(scan_string("br\n/"),                     	"Parse a bad string, error on ERRtest 9");
     expect_err(scan_string("div{'contenu'}"),               "Parse a bad string, error on ERRtest 10");
-    expect_err(scan_string("div{contenu avec espace}"),		"Parse a bad string, error on ERRtest 11");
 }
